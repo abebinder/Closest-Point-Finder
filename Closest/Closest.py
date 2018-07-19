e7f4a1f1d54c4f79da7bdfbd1855c5e4d6647f22
Abe Binder <fxhasthemovies@gmail.com>
2018-07-18 22:28:30 -0700

    new method

--- Closest/Closest.py
+++ Closest/Closest.py
@@ -3,91 +3,92 @@ import matplotlib.pyplot as plt
 
 random.seed(10)
 
+
 def solveClosestPoints(l):
     random.shuffle(l)
-    delta=distance(l[0],l[1])
-    top_pair=(0,1)
-    if(delta==0.0):
-        return(delta,top_pair)
+    delta = distance(l[0], l[1])
+    top_pair = (0, 1)
+    if (delta == 0.0):
+        return (delta, top_pair)
 
-    dict=MakeDictionary(l,delta,0)
-    for i in range(0,len(l)):
-        cell=whatCellAmIIn(l[i],delta)
-        for s in range(cell[0]-2, cell[0]+3):
-            for t in range(cell[1]-2, cell[1]+3):
-                j_list=dict.get((s,t))
-                if(j_list==None):
+    dict = MakeDictionary(l, delta, 0)
+    for i in range(0, len(l)):
+        cell = whatCellAmIIn(l[i], delta)
+        for s in range(cell[0] - 2, cell[0] + 3):
+            for t in range(cell[1] - 2, cell[1] + 3):
+                j_list = dict.get((s, t))
+                if (j_list == None):
                     continue
-                print( j_list )
+                print(j_list)
                 # if(len(j_list)>1):
                 #     print ("F")
                 # else:
                 #     print("T")
-                changed=False
+                changed = False
                 for j in j_list:
-                    dist=distance(l[i],l[j])
-                    if(dist<delta):
-                        changed=True
-                        delta=dist
-                        top_pair=(i,j)
-                        if(delta==0.0):
-                            return(delta,top_pair)
-                if(changed):
-                    dict=MakeDictionary(l,delta,i)
-                    cell=whatCellAmIIn(l[i], delta)
+                    dist = distance(l[i], l[j])
+                    if (dist < delta):
+                        changed = True
+                        delta = dist
+                        top_pair = (i, j)
+                        if (delta == 0.0):
+                            return (delta, top_pair)
+                if (changed):
+                    dict = MakeDictionary(l, delta, i)
+                    cell = whatCellAmIIn(l[i], delta)
 
-
-        if(cell in dict):
+        if (cell in dict):
             dict[cell].append(i)
         else:
-            dict[cell]=[i]
-    return (delta,top_pair)
+            dict[cell] = [i]
+    return (delta, top_pair)
 
 
 def MakeDictionary(l, delta, i):
-    dict={}
-    for j in range(0,i):
-        p=l[j]
-        a=whatCellAmIIn(p,delta)
-        if(a in dict):
+    dict = {}
+    for j in range(0, i):
+        p = l[j]
+        a = whatCellAmIIn(p, delta)
+        if (a in dict):
             dict[a].append(j)
         else:
-            dict[a]=[j]
+            dict[a] = [j]
     return dict
 
 
-def whatCellAmIIn(p,delta):
-    x=int(math.floor(p[0]/(delta/2)))
-    y=int(math.floor(p[1]/(delta/2)))
-    return (x,y)
-
+def whatCellAmIIn(p, delta):
+    x = int(math.floor(p[0] / (delta / 2)))
+    y = int(math.floor(p[1] / (delta / 2)))
+    return (x, y)
+def newMethod():
+    print("hi")
 
 
 def distance(p0, p1):
-    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
-
+    return math.sqrt((p0[0] - p1[0]) ** 2 + (p0[1] - p1[1]) ** 2)
 
 
 def randominputgenerator(n):
-    items_list=[]
+    items_list = []
     for i in range(n):
-        a=random.random()
-        b=random.random()
-        items_list.append((a,b))
+        a = random.random()
+        b = random.random()
+        items_list.append((a, b))
     return (items_list)
+
 
 def visualize(items_list):
     N = len(items_list)
     datax = [item[0] for item in items_list]
     datay = [item[1] for item in items_list]
 
-    #print data[:, 0]
-    #print data
+    # print data[:, 0]
+    # print data
     labels = ['{0}'.format(i) for i in range(N)]
 
-    plt.subplots_adjust(bottom = 0.1)
+    plt.subplots_adjust(bottom=0.1)
     plt.scatter(
-        datax, datay, #data[:, 0], data[:, 1],
+        datax, datay,  # data[:, 0], data[:, 1],
         cmap=plt.get_cmap('Spectral'))
 
     for label, x, y in zip(labels, datax, datay):
@@ -96,14 +97,15 @@ def visualize(items_list):
             xy=(x, y), xytext=(-10, 20),
             textcoords='offset points', ha='right', va='bottom',
             bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
-            arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0')
+            arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0')
         )
 
     plt.show()
 
+
 if __name__ == '__main__':
-    l=randominputgenerator(12)
-    #print(l)
-    #print("\n\n")
+    l = randominputgenerator(12)
+    # print(l)
+    # print("\n\n")
     print(solveClosestPoints(l))
     visualize(l)
